d72a0aa85ceeb429a7fad9b31c6db96435d9126e
Ciaran De Groot <ciaran.deg@gmail.com>
2023-10-22 12:27:32 -0600

    Add dynamic bindings

---
+++ config/hyprland/bindings.nix
@@ -0,0 +1,83 @@
+{lib, config, ...}: let
+  cfg = config.hyprland-config;
+in {
+  options.hyprland-config = {
+    bindings = lib.mkOption {
+      default = [
+        [ "bind" "SUPER" "M" "exit" "" ]
+        [ "bind" "SUPER" "Q" "killactive" "" ]
+        [ "bind" "SUPER" "F" "fullscreen" 0 ]
+        [ "bind" "SUPER" "K" "layoutmsg" "cycleprev" ]
+        [ "bind" "SUPER" "J" "layoutmsg" "cyclenext" ]
+        [ "bind" "SUPER SHIFT" "K" "layoutmsg" "swapprev" ]
+        [ "bind" "SUPER SHIFT" "J" "layoutmsg" "swapnext" ]
+        [ "bind" "SUPER" "Space" "layoutmsg" "swapwithmaster" ]
+        [ "bind" "SUPER" "H" "splitratio" (-0.1) ]
+        [ "bind" "SUPER" "L" "splitratio" 0.1 ]
+        [ "bind" "SUPER" 1 "workspace" 1 ]
+        [ "bind" "SUPER" 2 "workspace" 2 ]
+        [ "bind" "SUPER" 3 "workspace" 3 ]
+        [ "bind" "SUPER" 4 "workspace" 4 ]
+        [ "bind" "SUPER" 5 "workspace" 5 ]
+        [ "bind" "SUPER" 6 "workspace" 6 ]
+        [ "bind" "SUPER" 7 "workspace" 7 ]
+        [ "bind" "SUPER" 8 "workspace" 8 ]
+        [ "bind" "SUPER" 9 "workspace" 9 ]
+        [ "bind" "SUPER" 0 "workspace" 10 ]
+        [ "bind" "SUPER SHIFT" 1 "movetoworkspacesilent" 1 ]
+        [ "bind" "SUPER SHIFT" 2 "movetoworkspacesilent" 2 ]
+        [ "bind" "SUPER SHIFT" 3 "movetoworkspacesilent" 3 ]
+        [ "bind" "SUPER SHIFT" 4 "movetoworkspacesilent" 4 ]
+        [ "bind" "SUPER SHIFT" 5 "movetoworkspacesilent" 5 ]
+        [ "bind" "SUPER SHIFT" 6 "movetoworkspacesilent" 6 ]
+        [ "bind" "SUPER SHIFT" 7 "movetoworkspacesilent" 7 ]
+        [ "bind" "SUPER SHIFT" 8 "movetoworkspacesilent" 8 ]
+        [ "bind" "SUPER SHIFT" 9 "movetoworkspacesilent" 9 ]
+        [ "bind" "SUPER SHIFT" 0 "movetoworkspacesilent" 10 ]
+        [ "bind" "SUPER" "Semicolon" "workspace" "e+1" ]
+        [ "bind" "SUPER" "G" "workspace" "e-1" ]
+        [ "bind" "SUPER" "Tab" "workspace" "previous" ]
+        [ "bind" "SUPER SHIFT" "F" "togglefloating" "active" ]
+        [ "bind" "SUPER" "mouse:274" "toggleFloating" "active" ]
+        [ "bindm" "SUPER" "mouse:272" "movewindow" "" ]
+        [ "bindm" "SUPER" "mouse:273" "resizewindow" "" ]
+        [ "bind" "SUPER" "S" "pin" "active" ]
+        [ "bindl" "" "switch:Lid Switch" "exec" "swaylock" ]
+        [ "bind" "SUPER" "X" "exec" "swaylock" ]
+        [ "bind" "SUPER" "Minus" "exec" "brighter -i -10" ]
+        [ "bind" "SUPER" "Equal" "exec" "brighter -i 10" ]
+        [ "bind" "SUPER" "B" "exec" "killall -SIGUSR1 .waybar-wrapped" ]
+        [ "bind" "SUPER" "F11" "exec" "grim" ]
+        [ "bind" "SUPER SHIFT" "F11" "exec" "grim -g \"$(slurp)\"" ]
+        [ "bind" "SUPER SHIFT" "C" "exec" "cliphist list | wofi --show dmenu | cliphist decode | wl-copy" ]
+        [ "bind" "SUPER" "Return" "exec" "alacritty" ]
+        [ "bind" "SUPER" "R" "exec" "alacritty -e lf" ]
+        [ "bind" "SUPER SHIFT" "R" "exec" "alacritty -e htop" ]
+        [ "bind" "SUPER" "W" "exec" "chromium" ]
+        [ "bind" "SUPER" "D" "exec" "wofi --show run" ]
+        [ "bind" "SUPER SHIFT" "D" "exec" "discord" ]
+        [ "bind" "SUPER SHIFT" "S" "exec" "steam" ]
+        [ "bind" "SUPER" "C" "exec" "alacritty -t scratch-calc -e python3 -q" ]
+        [ "bind" "SUPER SHIFT" "P" "exec" "helvum" ]
+      ];
+    };
+    bindingsTranslated = lib.mkOption {
+      description = "Your hyprland bindings, in the format that hyprland.conf expects";
+      readOnly = true;
+      default = let
+        columns = ["command" "mods" "key" "dispatcher" "params"];
+        rawBindings = cfg.bindings;
+        bindingAttrs = map (bindingRow:
+        lib.lists.foldl (a: b: a // b) {} (
+          lib.lists.zipListsWith (col: bind: {${col} = bind;}) columns bindingRow)
+        ) rawBindings;
+        configLines = map (
+          bind: "${bind.command} = ${bind.mods}, ${bind.key}, ${bind.dispatcher}, ${bind.params}"
+        ) bindingAttrs;
+      in lib.strings.concatStringsSep "\n" configLines;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+  };
+}
--- config/hyprland/default.nix
+++ config/hyprland/default.nix
@@ -1,6 +1,8 @@
 {lib, config, ...}: let
   cfg = config.hyprland-config;
 in {
+  imports = [./bindings.nix];
+
   options.hyprland-config = {
     monitorConfig = lib.mkOption {
       description = "Monitor configuration for hyprland.conf";
@@ -86,100 +88,7 @@ in {
         windowrule = pin,title:scratch-calc
         windowrule = size 600 400,title:scratch-calc
         windowrule = center,title:scratch-calc
-        
-        # Kill Hyprland
-        bind = $mainMod, M, exit, 
-        
-        # Kill window
-        bind = $mainMod, Q, killactive, 
-        
-        # Make window fullscreen
-        bind = $mainMod, F, fullscreen, 0
-        
-        # Move focus
-        bind = $mainMod, K, layoutmsg, cycleprev
-        bind = $mainMod, J, layoutmsg, cyclenext
-        
-        # Rearrange window stack
-        bind = $mainMod SHIFT, K, layoutmsg, swapprev
-        bind = $mainMod SHIFT, J, layoutmsg, swapnext
-        bind = $mainMod, Space, layoutmsg, swapwithmaster
-        
-        # Adjust window split
-        bind = $mainMod, H, splitratio, -0.1
-        bind = $mainMod, L, splitratio, 0.1
-        
-        # Switch workspaces with mainMod + [0-9]
-        bind = $mainMod, 1, workspace, 1
-        bind = $mainMod, 2, workspace, 2
-        bind = $mainMod, 3, workspace, 3
-        bind = $mainMod, 4, workspace, 4
-        bind = $mainMod, 5, workspace, 5
-        bind = $mainMod, 6, workspace, 6
-        bind = $mainMod, 7, workspace, 7
-        bind = $mainMod, 8, workspace, 8
-        bind = $mainMod, 9, workspace, 9
-        bind = $mainMod, 0, workspace, 10
-        
-        # Move active window to a workspace with mainMod + SHIFT + [0-9]
-        bind = $mainMod SHIFT, 1, movetoworkspacesilent, 1
-        bind = $mainMod SHIFT, 2, movetoworkspacesilent, 2
-        bind = $mainMod SHIFT, 3, movetoworkspacesilent, 3
-        bind = $mainMod SHIFT, 4, movetoworkspacesilent, 4
-        bind = $mainMod SHIFT, 5, movetoworkspacesilent, 5
-        bind = $mainMod SHIFT, 6, movetoworkspacesilent, 6
-        bind = $mainMod SHIFT, 7, movetoworkspacesilent, 7
-        bind = $mainMod SHIFT, 8, movetoworkspacesilent, 8
-        bind = $mainMod SHIFT, 9, movetoworkspacesilent, 9
-        bind = $mainMod SHIFT, 0, movetoworkspacesilent, 10
-        
-        # Rotate through existing workspaces
-        bind = $mainMod, Semicolon, workspace, e+1
-        bind = $mainMod, G, workspace, e-1
-        
-        # Switch to most recently viewed workspace
-        bind = $mainMod, Tab, workspace, previous
-        
-        # Toggle current window floating
-        bind = $mainMod SHIFT, F, togglefloating, active
-        bind = $mainMod, mouse:274, toggleFloating, active
-        
-        # Move/resize windows with mainMod + LMB/RMB and dragging
-        bindm = $mainMod, mouse:272, movewindow
-        bindm = $mainMod, mouse:273, resizewindow
-        
-        # Pin active window
-        bind = $mainMod, S, pin, active
-        
-        # Screen locker
-        bindl=,switch:Lid Switch,exec,swaylock
-        bind = $mainMod, X, exec, swaylock
-        
-        # Screen brightness
-        bind = $mainMod, Minus, exec, brighter -i -10
-        bind = $mainMod, Equal, exec, brighter -i 10
-        
-        # Toggle waybar
-        bind = $mainMod, B, exec, killall -SIGUSR1 .waybar-wrapped
-        
-        # Screenshots
-        bind = $mainMod, F11, exec, grim
-        bind = $mainMod SHIFT, F11, exec, grim -g "$(slurp)"
-        
-        # Clipboard manager
-        bind = $mainMod SHIFT, C, exec, cliphist list | wofi --show dmenu | cliphist decode | wl-copy
-        
-        # Launch programs
-        bind = $mainMod, Return, exec, alacritty
-        bind = $mainMod, R, exec, alacritty -e lf
-        bind = $mainMod SHIFT, R, exec, alacritty -e htop
-        bind = $mainMod, W, exec, chromium
-        bind = $mainMod, D, exec, wofi --show run
-        bind = $mainMod SHIFT, D, exec, discord
-        bind = $mainMod SHIFT, S, exec, steam
-        bind = $mainMod, C, exec, alacritty -t scratch-calc -e python3 -q
-        bind = $mainMod SHIFT, P, exec, helvum
-      '';
+      '' + cfg.bindingsTranslated;
     };
   };
 }
