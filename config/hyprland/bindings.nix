d04bf33a9baf9058fa53a45967fa5b9f8399ac58
Ciaran De Groot <ciaran.deg@gmail.com>
2023-10-22 13:25:20 -0600

    Add comments to bindings

--- config/hyprland/bindings.nix
+++ config/hyprland/bindings.nix
@@ -8,16 +8,29 @@ in {
   options.hyprland-config.bindings = {
     keyboard = lib.mkOption {
       default = [
+        # Kill Hyprland
         ["SUPER" "M" "exit" ""]
+
+        # Kill window
         ["SUPER" "Q" "killactive" ""]
+
+        # Make window fullscreen
         ["SUPER" "F" "fullscreen" "0"]
+
+        # Move focus
         ["SUPER" "K" "layoutmsg" "cycleprev"]
         ["SUPER" "J" "layoutmsg" "cyclenext"]
+
+        # Rearrange window stack
         ["SUPER SHIFT" "K" "layoutmsg" "swapprev"]
         ["SUPER SHIFT" "J" "layoutmsg" "swapnext"]
         ["SUPER" "Space" "layoutmsg" "swapwithmaster"]
+
+        # Adjust window split
         ["SUPER" "H" "splitratio" "-0.1"]
         ["SUPER" "L" "splitratio" "0.1"]
+
+        # Switch between workspaces
         ["SUPER" "1" "workspace" "1"]
         ["SUPER" "2" "workspace" "2"]
         ["SUPER" "3" "workspace" "3"]
@@ -28,6 +41,8 @@ in {
         ["SUPER" "8" "workspace" "8"]
         ["SUPER" "9" "workspace" "9"]
         ["SUPER" "0" "workspace" "10"]
+
+        # Move active window between workspaces
         ["SUPER SHIFT" "1" "movetoworkspacesilent" "1"]
         ["SUPER SHIFT" "2" "movetoworkspacesilent" "2"]
         ["SUPER SHIFT" "3" "movetoworkspacesilent" "3"]
@@ -38,19 +53,39 @@ in {
         ["SUPER SHIFT" "8" "movetoworkspacesilent" "8"]
         ["SUPER SHIFT" "9" "movetoworkspacesilent" "9"]
         ["SUPER SHIFT" "0" "movetoworkspacesilent" "10"]
+
+        # Rotate through existing workspaces
         ["SUPER" "Semicolon" "workspace" "e+1"]
         ["SUPER" "G" "workspace" "e-1"]
+
+        # Switch to most recently viewed workspace
         ["SUPER" "Tab" "workspace" "previous"]
+
+        # Toggle current window floating
         ["SUPER SHIFT" "F" "togglefloating" "active"]
         ["SUPER" "mouse:274" "toggleFloating" "active"]
+
+        # Pin active window
         ["SUPER" "S" "pin" "active"]
+
+        # Screen locker
         ["SUPER" "X" "exec" "swaylock"]
+
+        # Screen brightness
         ["SUPER" "Minus" "exec" "brighter -i -10"]
         ["SUPER" "Equal" "exec" "brighter -i 10"]
+
+        # Toggle waybar
         ["SUPER" "B" "exec" "killall -SIGUSR1 .waybar-wrapped"]
+
+        # Screenshots
         ["SUPER" "F11" "exec" "grim"]
         ["SUPER SHIFT" "F11" "exec" "grim -g \"$(slurp)\""]
+
+        # Clipboard manager
         ["SUPER SHIFT" "C" "exec" "cliphist list | wofi --show dmenu | cliphist decode | wl-copy"]
+
+        # Launch programs
         ["SUPER" "Return" "exec" "alacritty"]
         ["SUPER" "R" "exec" "alacritty -e lf"]
         ["SUPER SHIFT" "R" "exec" "alacritty -e htop"]
@@ -64,12 +99,14 @@ in {
     };
     mouse = lib.mkOption {
       default = [
+        # Move/resize windows with mouse
         ["SUPER" "mouse:272" "movewindow"]
         ["SUPER" "mouse:273" "resizewindow"]
       ];
     };
     switches = lib.mkOption {
       default = [
+        # Screen locker
         ["" "switch:Lid Switch" "exec" "swaylock"]
       ];
     };
