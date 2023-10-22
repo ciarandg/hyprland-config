d14f309d44b0cfb0c66633a26ac92638ae6e85b5
Ciaran De Groot <ciaran.deg@gmail.com>
2023-10-21 23:26:19 -0600

    Remove default packages

--- flake.nix
+++ flake.nix
@@ -2,10 +2,5 @@
   description = "Ciaran's hyprland config";
 
   outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
   };
 }
