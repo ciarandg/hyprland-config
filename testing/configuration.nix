00910738d92035d350a59b6d7c765ac6f51ef008
Ciaran De Groot <ciaran.deg@gmail.com>
2023-11-05 17:02:59 -0700

    Remove DE from nixos testing config

--- testing/configuration.nix
+++ testing/configuration.nix
@@ -3,11 +3,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  services.xserver.enable = true;
-
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
   users.users.test = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
